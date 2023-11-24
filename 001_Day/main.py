837e0baa41f0366e5fc273c78e999f99e3b67d10
Joel Lovely <Joellovely0717@gmail.com>
2023-11-24 16:34:34 -0600

    1st Day Completed

--- 001_Day/main.py
+++ 001_Day/main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+#This is a Brand Name Generator
+print("Welcome To The First Program, For The Python Course Developed By Dr. Angela Yu")
+variable_1 = input("Where Did You Grow Up ? (city)")
+variable_2 = input("What Is The Name Of Your Pet? ")
+Variable_3 = variable_1 + " " + variable_2
+print("Your Band Name Should be " + Variable_3 + "\n")
