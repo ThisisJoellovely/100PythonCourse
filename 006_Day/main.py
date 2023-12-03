2495fab208777bb05e9a4661c567be5401d5f185
Joel Lovely <Joellovely0717@gmail.com>
2023-12-03 17:23:53 -0600

    Day 7 Completed

---
+++ 006_Day/main.py
@@ -0,0 +1,19 @@
+# Using This URL - You Can Run The Code
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#Final Code for Day 9
+
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+
+    
+#while not at_goal():
+#    if right_is_clear():
+#        turn_right()
+#        move()
+#    elif front_is_clear():
+#        move()
+#    else:
+#        turn_left()
