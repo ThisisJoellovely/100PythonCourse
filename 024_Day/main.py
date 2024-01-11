8dae8225524e5898ec08c9d86ee164a1c6f57815
Joel Lovely <Joellovely0717@gmail.com>
2024-01-11 01:33:33 -0600

    Day 24 Completed

--- 024_Day/main.py
+++ 024_Day/main.py
@@ -11,7 +11,7 @@ import os
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 STARTING_LETTER = "/Users/lovely/Documents/100_DaysOfProgramming/024_Day/Input/Letters/starting_letter.txt"
-DESTINATION_LETTER = "/Users/lovely/Documents/100_DaysofProgramming/024_Day/Ouput/ReadyToSend"
+DESTINATION_LETTER = "/Users/lovely/Documents/100_DaysofProgramming/024_Day/Output/ReadyToSend/"
 NAMES_PATH = "/Users/lovely/Documents/100_DaysOfProgramming/024_Day/Input/Names/invited_names.txt"
 
 #Global Variables 
@@ -20,11 +20,11 @@ names = []
 def create_letter(filename ,name):
     """"This Function will take the parameter of a name and pass name into subjgated letter and create a file and set in the output file"""        
     file_path = DESTINATION_LETTER + filename
-    with open(STARTING_LETTER) as file:
-        header = file.readline(11)
-        header.replace("[name]" , name)
-        content = file.read()  
-        with open(file_path,"w") as export:
+    with open(STARTING_LETTER, 'r') as file:
+        header = file.readline()
+        header = header.replace("[name]", name)
+        content = file.read()
+        with open(file_path, 'w') as export:
             export.write(header)
             export.write(content)
 
@@ -32,16 +32,22 @@ def get_names(filename):
     """This is going retrive lists and add to a global list defined in the main program"""
     global names
     with open(filename) as file:
-        while file.closed != True:
-            content = file.readlines()
+        while file.readable():
+            content = file.readline()
+            content.rstrip()
+            if(content == ''):
+                break
             names.append(content)
+            
+
 
 get_names(NAMES_PATH)
 for i in range(len(names)):
-    current_name = names[i]
-    filename = "Finished_" + current_name
+    current_name = names[i].replace('\n','')
+    filename = f"Finished_{current_name}.txt"
     create_letter(filename , current_name)
-print("Finished")
+
+
 
 
 
