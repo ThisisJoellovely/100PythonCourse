d8a08f5dc790715ff062ecf2d33b3a598a0e2452
Joel Lovely <Joellovely0717@gmail.com>
2024-05-09 20:00:14 -0500

    Final Part of Day 63

---
+++ 063_Day/SQLlite_Demo/main.py
@@ -0,0 +1,41 @@
+# import sqlite3
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor() 
+
+# #cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+from flask import Flask, render_template, request, redirect 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column 
+from sqlalchemy import Integer, String, Float
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app=app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False) 
+
+    def __repr__(self):
+        return f'<book {self.title}>'
+    
+with app.app_context():
+    db.create_all()
+
+
+with app.app_context():
+    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
+
