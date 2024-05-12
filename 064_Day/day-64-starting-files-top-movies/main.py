9221f1a9617e6e8f895611877f3920d60a997a25
Joel Lovely <Joellovely0717@gmail.com>
2024-05-12 16:23:34 -0500

    1:39 at Arlington Heights Memorial Library

--- 064_Day/day-64-starting-files-top-movies/main.py
+++ 064_Day/day-64-starting-files-top-movies/main.py
@@ -1,4 +1,5 @@
 import os
+import requests  
 from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
@@ -9,7 +10,7 @@ from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, StringField, SubmitField
 from wtforms.validators import DataRequired
-import requests
+
 
 '''
 Red underlines? Install the required packages first: 
@@ -24,41 +25,49 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 load_dotenv("/Users/lovely/Documents/Udemy/SECRET_API_KEYS/064_Day/.env")
-TOP_10_MOVIES_API_KEY = os.getenv("TOP_10_MOVIES_API_KEY")
+
+# Constants
+THE_MOVIES_DATABASE_API_KEY = os.getenv("TOP_10_MOVIES_API_KEY")
+THE_MOVIES_DATABASE_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://///Users/lovely/Documents/Udemy/100_DaysOfProgramming/064_Day/day-64-starting-files-top-movies/movies.db"
-
 Bootstrap5(app)
 
+# Database Initalization
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app=app)
 
+
 class Movie(db.Model):
     """This is the Components of a Movies Table by row"""
-    id : Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True)
-    title : Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    year : Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    rating : Mapped[float] = mapped_column(Float, nullable=False)
-    ranking : Mapped[int] = mapped_column(Integer, nullable=False)
-    review : Mapped[str] = mapped_column(String, nullable=False)
-    img_url : Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 with app.app_context():
     """This creates the desired Movies table listed above """
     db.create_all()
 
 def top_10_list():
+    """This Creates a Scalar Object in order of the ID primiary Key"""
     movies = db.session.execute(select(Movie).order_by(Movie.id)).scalars()
     return movies
     
-
+# Class Functions
 class EditForm(FlaskForm):
     """Creating Easy to use form uisng WTF Moudle"""
     edit_rating = DecimalField('ex: 10.0',places=2)
@@ -69,6 +78,34 @@ class AddMovieTitle(FlaskForm):
     """Creating Easy to use form using WTF Module"""
     add_movie_title = StringField("Movie Title", validators=[DataRequired()])
     add_movie_title_submiti = SubmitField('Add Movie')
+
+
+class TopMoviesDataBase(requests.Session):
+
+    def __init__(self):
+        super().__init__()
+
+    def makeRequestAddCall(self, the_movies_database_parameter, url=THE_MOVIES_DATABASE_URL):
+            response = self.get(url=url, params=the_movies_database_parameter)
+            data = response.json()['results']
+            return data
+    
+    def makeRequestFindCall(self, the_movies_database_parameter, url):
+            response = self.get(url=url, params=the_movies_database_parameter)
+            data = response.json()
+            new_movie = Movie(
+                 title= data["title"],
+                 year= data["release_data"].split("-")[0],
+                 img_url = f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+                 description= data["overview"]
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return new_movie
+            
+            
+            
+            
 
 
 
@@ -125,12 +162,34 @@ def delete():
     db.session.commit()
     return redirect(url_for('home'))
 
+
 @app.route("/add", methods=["GET", "POST"])
-def add():
+def add_movies():
     form = AddMovieTitle()
+    if form.validate_on_submit():
+        top_movies_database = TopMoviesDataBase()
+        movie_title = form.add_movie_title.data
+        the_movies_database_parameter = {
+        "api_key": THE_MOVIES_DATABASE_API_KEY,
+        "query": movie_title
+        }
+        data = top_movies_database.makeRequestAddCall(the_movies_database_parameter=the_movies_database_parameter)
+        return render_template("select.html", options=data)
     return render_template("add.html", form=form)        
 
-
+@app.route("/find")
+def find_movie():
+        movieTMDB = request.args.get("id")  
+        if  movieTMDB:
+            top_movies_database = TopMoviesDataBase()
+            the_movies_database_parameter = {
+            "api_key": THE_MOVIES_DATABASE_API_KEY,
+            "language" : "en-US"
+            }
+            movieTMDB_url  = f"{THE_MOVIES_DATABASE_URL}/{movieTMDB}"
+            movieTMDB_return = top_movies_database.makeRequestFindCall(url=movieTMDB_url,the_movies_database_parameter=the_movies_database_parameter)
+            return redirect(url_for("edit", id=movieTMDB_return.id))
+            
 
 
 
